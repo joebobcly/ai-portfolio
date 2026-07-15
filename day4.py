1808d42c5657b7f88c929937ef1d4b94262e4e6b
joebobcly <joebobcly@gmail.com>
2026-07-15 15:36:49 -0500

    Complete Day 4 - Dictionaries

---
+++ day4.py
@@ -0,0 +1,11 @@
+person = {
+    "name": "Joe",
+    "company": "MongoDB",
+    "role": "Director",
+    "favorite_ai_tool": "Gemini",
+    "city": "Austin"
+}
+
+print(person["name"])
+print(person["favorite_ai_tool"])
+print(person["city"])
---
+++ day4_example1.py
@@ -0,0 +1,7 @@
+lead = {
+    "company": "MongoDB",
+    "employees": 7500,
+    "industry": "Database",
+    "headquarters": "New York"
+}
+print(lead["company"])
