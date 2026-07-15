92a63b0cf5dc31b19df5356c0315dc85390c9dbb
joebobcly <joebobcly@gmail.com>
2026-07-14 19:04:39 -0500

    Complete Day 3 - loops and functions

---
+++ functions.py
@@ -0,0 +1,12 @@
+animals = [
+    "Dog",
+    "Cat",
+    "Elephant",
+    "Giraffe"
+]
+
+def favorite(animal):
+    print(f"My favorite animal is {animal}")
+
+for animal in animals:
+    favorite(animal)
---
+++ loops.py
@@ -0,0 +1,8 @@
+customers = [
+    "Microsoft",
+    "Adobe",
+    "Cisco",
+]
+
+for customer in customers:
+    print (f"Sending email to {customer}")
